c84041107a2415758c1c7c725a4c3f5a4d5d5e4d
Oleksandr Kondratiuk <alex.sledg@gmail.com>
2025-10-21 06:33:15 +0200

    update inc/widgets.php

---
+++ inc/widgets.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * 
+ * Savana - Ecommerce WordPress Theme
+ * Widget Registration
+ * 
+ * @encoding     UTF-8
+ * @copyright    Copyright (C) 2025 IDLayers (http://idlayers.com). All rights reserved.
+ * @license      Envato Standard License http://themeforest.net/licenses/standard?ref=IDLayers
+ * @author       Oleksandr Kondratiuk (alex.sledg@gmail.com)
+ * @support      alex.sledg@gmail.com
+ * 
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ * Register widget areas
+ */
+function idltm_widgets_init() {
+    
+    // Primary Sidebar
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Primary Sidebar', 'New_theme' ),
+            'id'            => 'sidebar-1',
+            'description'   => esc_html__( 'Add widgets here to appear in your sidebar.', 'New_theme' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+    
+    // Footer Widget Area 1
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer 1', 'New_theme' ),
+            'id'            => 'footer-1',
+            'description'   => esc_html__( 'Add widgets here to appear in footer column 1.', 'New_theme' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+    
+    // Footer Widget Area 2
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer 2', 'New_theme' ),
+            'id'            => 'footer-2',
+            'description'   => esc_html__( 'Add widgets here to appear in footer column 2.', 'New_theme' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+    
+    // Footer Widget Area 3
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer 3', 'New_theme' ),
+            'id'            => 'footer-3',
+            'description'   => esc_html__( 'Add widgets here to appear in footer column 3.', 'New_theme' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+    
+    // Footer Widget Area 4
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer 4', 'New_theme' ),
+            'id'            => 'footer-4',
+            'description'   => esc_html__( 'Add widgets here to appear in footer column 4.', 'New_theme' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+    
+    // WooCommerce Shop Sidebar
+    if ( class_exists( 'WooCommerce' ) ) {
+        register_sidebar(
+            array(
+                'name'          => esc_html__( 'Shop Sidebar', 'New_theme' ),
+                'id'            => 'sidebar-shop',
+                'description'   => esc_html__( 'Add widgets here to appear in WooCommerce shop sidebar.', 'New_theme' ),
+                'before_widget' => '<section id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</section>',
+                'before_title'  => '<h2 class="widget-title">',
+                'after_title'   => '</h2>',
+            )
+        );
+    }
+}
+add_action( 'widgets_init', 'idltm_widgets_init' );
